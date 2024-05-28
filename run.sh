51cd65a11cff852e1daf0f84fa7c0611c5fc876b
dimi <165000549+dimibre@users.noreply.github.com>
2024-05-28 15:40:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-bbe414f6-0ab9-4713-bfa2-7986540035b6/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-cf5c30f3-af1c-4bec-b0ad-dbeb0c4f0f59/)
 
 
 echo "$curl_output" >> Update.tsx
